88787ed9c3fc83f8703a77d005c4c9280fa71a4d
Clucas0311 <clucas0311@gmail.com>
2021-06-05 17:44:42 -0400

    LinkedList Cycle

---
+++ LinkedList/linked_list_cycle.py
@@ -0,0 +1,64 @@
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ Approach:
+ This algorithim uses the fast and slow pointer technique
+ slow will be the head 
+ fast will be at the head
+ So the we will iterate as long as thier is positions for the fast pointer
+ we will be moving the fast pointer two steps ahead so we will need to check 
+ if the current fast position and its next are not none
+ if this is true 
+ slow will move on position 
+ and fast will move two positions 
+
+ GOAL: Think of two cars driving in a loop they will eventually collide if they are constantly 
+ moving in the circle
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+"""
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return None
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
